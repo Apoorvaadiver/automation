514f7d6cb66bcf0e8d6d91732ca4d812890fdd4e
Dell <Dell@DESKTOP-EFVD5B9>
2023-02-07 14:26:08 +0530

    review

---
+++ Selenium/src/qsp/screen_shot.java
@@ -0,0 +1,25 @@
+package qsp;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class screen_shot {
+public static void main(String[] args) throws InterruptedException, IOException {
+	System.setProperty("webdriver.chrome.driver","./softwares/chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+	Thread.sleep(2000);
+TakesScreenshot	ts =(TakesScreenshot)driver;
+File src= ts.getScreenshotAs(OutputType.FILE);
+File dst = new File("C:\\Users\\Dell\\Pictures\\sreenshot1\\screen1.jpeg");
+FileUtils.copyFile(src, dst);
+	
+	
+} 
+}
